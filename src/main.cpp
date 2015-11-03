ae85f4021e10afbc17bd4918e5da0700cd17a6eb
sebastian.schwank <sebastian.schwank@googlemail.com>
2015-11-03 06:44:08 -0500

    ... see comment before

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,21 @@
+/* Basic IO (@Param: "-Files:" ):
+				Basic acess is given by passing pairs of .wav Files to the Application via Commandline:
+					
+					- The first one is the given INPUT File which is the "source"-Signal for the "calculation-graphs" beeing generaterated
+					- The second one is the giiven TARGET File which is the target-Signal for the "calculation-graphs" the Mean-Error per Sample is defined as fittness funcion
+					
+					Syntax: DSPGen.out -Files: [Path to the INPUT-File ; Path to the Target File][Path to the INPUT-File ; Path to the Target File] ... 
+					
+					Note:	- Multible access to the same File is possible
+							- only .wav-Files are supported
+							- All Input Files must have the same length !!!
+							- All Target Files must have the same length !!!
+*/
+
+
 #include <iostream>
 
-int main(){
+int main(int argc, char *argv[]){
 	
 	std::cout << "Hallo Cloud ! \n";
 	
